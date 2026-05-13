602528fb0a1e3ba506615f9d0ad3345a1745e57f
Smooth <92906509+VladimirGladky@users.noreply.github.com>
2026-05-14 01:54:15 +0300

    feat: CLI entrypoint orchestrating config, client and output

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"TestDataMining/internal/config"
+	"TestDataMining/internal/lenta"
+	"TestDataMining/internal/output"
+)
+
+func main() {
+	envPath := flag.String("env", ".env", "path to .env file")
+	outPath := flag.String("out", "out/products.json", "path to output JSON file")
+	flag.Parse()
+
+	cfg, err := config.Load(*envPath)
+	if err != nil {
+		log.Fatalf("config: %v", err)
+	}
+
+	client, err := lenta.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("client: %v", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	results := make([]lenta.CategoryResult, 0, len(cfg.SelectionIDs))
+	for _, id := range cfg.SelectionIDs {
+		log.Printf("scraping selection %d via proxy...", id)
+		res, err := client.ScrapeSelection(ctx, id)
+		if err != nil {
+			log.Fatalf("selection %d: %v", id, err)
+		}
+		log.Printf("selection %d done: %d products", id, len(res.Products))
+		results = append(results, *res)
+	}
+
+	if err := output.WriteJSON(*outPath, results); err != nil {
+		log.Fatalf("write output: %v", err)
+	}
+
+	log.Printf("done. wrote %s", *outPath)
 }
